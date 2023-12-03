7bb040a8ad8162a0ddcfa8eca70605205efd7ea2
ingk-18 <readog1224@yahoo.co.jp>
2023-12-03 17:58:30 +0900

    命名練習

---
+++ practice.js
@@ -0,0 +1,65 @@
+
+
+// スネークケースに直す
+const header_height = 80;
+const user_message = "hello world";
+
+// パスカルケースに直す
+class UserData {
+  constructor() {
+    this.id = "12345";
+  }
+}
+
+class GamePlayer {
+  constructor() {
+    this.score = 0;
+  }
+}
+
+// キャメルケースに直す
+const bookItem = {
+  id: '1',
+  title: 'Hello World'
+};
+
+function addActiveClass() {...};
+
+
+
+// 元旦
+const NEW_YEARS_DAY = "1月1日";
+
+// 曜日を格納した配列
+const DAY_OF_THE_WEEK = ["日", "月", "火", "水", "木", "金", "土"];
+
+// 3割引の商品の値段
+function xxx(number) {
+  return number * (1 - 0.3);
+}
+
+// 数値配列の合計値を取得
+function xxx(array) {
+  return array.reduce(function (a, b) {
+    return a + b;
+  });
+}
+
+// お気に入りのスポーツのクラス
+class xxx {
+  constructor(name) {
+    this.name = name;
+  }
+  showInfo() {
+    console.log(`私の好きなスポーツは${this.name}です。`);
+  }
+}
+
+// 惑星
+class xxx {
+  constructor(name) {
+    this.name = name;
+  }
+  // 地球からの距離を返す
+  xxx() {...}
+}
