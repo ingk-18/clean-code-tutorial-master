ac68d47431e616e2a6731d6e17d1e456449eedc2
ingk-18 <readog1224@yahoo.co.jp>
2024-06-23 21:52:48 +0900

    条件分岐をcallback関数に変更

--- practice.js
+++ practice.js
@@ -1,65 +1,30 @@
 
 
-// スネークケースに直す
-const header_height = 80;
-const user_message = "hello world";
 
-// パスカルケースに直す
-class UserData {
-  constructor() {
-    this.id = "12345";
-  }
+function createVegetableText(name) {
+  return `${name}は洗って皮を剥き、食べやすい大きさにカットします。`;
+}
+function createFruitText(name) {
+  return `${name}は皮を剥いてお皿に盛り付けます。`;
+}
+function createMeatText(name) {
+  return `${name}は一口大に切り、炒めます。`;
 }
 
-class GamePlayer {
-  constructor() {
-    this.score = 0;
-  }
+function getRecipe(obj) {
+  return obj.callback(obj.name)
 }
 
-// キャメルケースに直す
-const bookItem = {
-  id: '1',
-  title: 'Hello World'
-};
-
-function addActiveClass() {...};
-
-
-
-// 元旦
-const NEW_YEARS_DAY = "1月1日";
-
-// 曜日を格納した配列
-const DAY_OF_THE_WEEK = ["日", "月", "火", "水", "木", "金", "土"];
-
-// 3割引の商品の値段
-function xxx(number) {
-  return number * (1 - 0.3);
-}
-
-// 数値配列の合計値を取得
-function xxx(array) {
-  return array.reduce(function (a, b) {
-    return a + b;
-  });
-}
-
-// お気に入りのスポーツのクラス
-class xxx {
-  constructor(name) {
+class Vegetable{
+  constructor(name, callback) {
     this.name = name;
-  }
-  showInfo() {
-    console.log(`私の好きなスポーツは${this.name}です。`);
+    this.callback = callback;
   }
 }
 
-// 惑星
-class xxx {
-  constructor(name) {
-    this.name = name;
-  }
-  // 地球からの距離を返す
-  xxx() {...}
-}
+const recipes = [];
+recipes.push(getRecipe(new Vegetable('にんじん', createVegetableText )));
+recipes.push(getRecipe(new Vegetable('ぶどう', createFruitText )));
+recipes.push(getRecipe(new Vegetable('鶏肉', createMeatText)));
+console.log(recipes);
+
