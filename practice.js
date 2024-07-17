b0bca19ed5f2775d39ade4810142789880760ec0
ingk-18 <readog1224@yahoo.co.jp>
2024-07-17 18:53:17 +0900

    共通化を練習

--- practice.js
+++ practice.js
@@ -1,30 +1,54 @@
+Emails = {
+
+  1 : '千代田',
+  2 : '中央',
+  3 : '港'
+};
 
 
+console.log(Emails[1]);
 
-function createVegetableText(name) {
-  return `${name}は洗って皮を剥き、食べやすい大きさにカットします。`;
-}
-function createFruitText(name) {
-  return `${name}は皮を剥いてお皿に盛り付けます。`;
-}
-function createMeatText(name) {
-  return `${name}は一口大に切り、炒めます。`;
-}
-
-function getRecipe(obj) {
-  return obj.callback(obj.name)
+function getUserName(user) {
+  if (user === undefined) return '名無し';
+  return user.name ?? 'なし';
 }
 
-class Vegetable{
-  constructor(name, callback) {
-    this.name = name;
-    this.callback = callback;
+console.log(getUserName('hoge'));
+
+function isPrimeNumber(n) {
+  if (n === 1) return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+for (let i = 1; i <= 30; i++) {
+  console.log(i, isPrimeNumber(i));
+}
+
+function isCorrectInput(member) {
+  if (member.id === undefined) {
+    console.error("idがありません");
+    return false;
+  }
+
+  if (member.name === undefined) {
+    console.error("名前が入力されていません");
+    return false;
+  }
+
+  if (member.age) {
+    if (member.email) {
+      return true;
+    } else {
+      console.error("メールアドレスが入力されていません");
+      return false;
+    }
+  } else {
+    console.error("年齢が入力されていません");
+    return false;
   }
 }
-
-const recipes = [];
-recipes.push(getRecipe(new Vegetable('にんじん', createVegetableText )));
-recipes.push(getRecipe(new Vegetable('ぶどう', createFruitText )));
-recipes.push(getRecipe(new Vegetable('鶏肉', createMeatText)));
-console.log(recipes);
-
